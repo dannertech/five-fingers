dbbba73096c8175e8892fd54d9bd98311ee27fa1
dannertech <danner@danner.tech>
2019-06-12 20:16:16 -0400

    knife moves x and I also added the cut hand and cut fingers

--- Assets/MoveKnife.cs
+++ Assets/MoveKnife.cs
@@ -4,24 +4,27 @@ using UnityEngine;
 
 public class MoveKnife : MonoBehaviour {
 	private float timer = 0.0f;
-	private bool timeLimit = true;
+	private bool timeLimit = false;
 	private string nextAnimation = "finger five animation";
-	private Animation anim;
+	private Animator anim;
 	public float speed = 50f;
 	string level = "level three";
 
+
 	 void Start()
 	{
-		//Vector3 temp = new Vector3(0, -5.0f * Time.deltaTime, 0);
-		//transform.position += temp;
-		anim = this.GetComponent<Animation> ();
+
+	anim = GetComponent<Animator> ();
 	}  
 	
 	// Update is called once per frame
 	void Update () {
+
+		
 		if (timeLimit != true) {
-			
-			transform.Translate (0, speed * Time.deltaTime, 0);
+			Vector3 goingUp = new Vector3 (0, speed * Time.deltaTime, 0);
+			transform.position += goingUp;
+
 
 			if (Input.GetKeyDown ("space")) {
 				timeLimit = true;
@@ -53,28 +56,9 @@ public class MoveKnife : MonoBehaviour {
 			anim.Play ("MoveKnifeX5");
 			nextAnimation = "finger one animation";
 			break;
-		}
+		} 
 
 
-		//anim.Play ("MoveKnifeX");
 
-		//anim.Play ("MoveKnifeX2");
-	
-/*		switch(level) {
-		case "level one":
-		 	speed = 10f;
-			print("level one");
-			break;
-		case "level two":
-			speed = 20f;
-			print("level two");
-			break;
-		case "level three":
-			speed = 30f;
-			print("level three");
-			break; 
-		} */
-	//	transform.Translate (0, Time.deltaTime, 0);
-	//	transform.Translate (Vector3.down * Time.deltaTime);
 	}
 }
